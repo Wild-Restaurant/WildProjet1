e3f82473037f0ab89abf30b9dbf638f1f21f7abb
theo anache <theoo.anache@gmail.com>
2022-10-05 22:40:17 +0200

    animation chargement page

---
+++ script.js
@@ -0,0 +1,21 @@
+// const chargement = document.querySelector(".chargement");
+
+
+// setTimeout(function () {
+//     $(".chargement");
+// }, 3000);
+
+// if (chargement === )
+// chargement.display = none;
+// else
+
+// $(document).ready(function () {
+//     $(chargement).fadeOut(5000, function () {
+//         $(nosProduits).show(1000);
+//     });
+// })
+
+
+setTimeout(function () {
+    $('.chargement').fadeOut();
+}, 4000);
